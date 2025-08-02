43cf4bdc13ac18dc33be2867fe45594992a4aa12
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-08-02 17:03:38 -0400

    Added markdown-it plugins to simplify and expand markdown capabilities

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const markdownItFootnote = require("markdown-it-footnote");
+const markdownItMark = require("markdown-it-mark");
+const markdownItSub = require("markdown-it-sub");
+const markdownItSup = require("markdown-it-sup");
+const markdownItContainer = require("markdown-it-container");
+
+
 module.exports = function(eleventyConfig) {
-  // Add Passthrough Copy for CSS & JS
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/js"); 
   eleventyConfig.addPassthroughCopy("src/_redirects");
@@ -9,6 +17,30 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("all", function(collection) {
     return collection.getAll();
   });
+
+  const options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  }
+
+  const markdownLib = markdownIt(options)
+    .use(markdownItAttrs)
+    .use(markdownItFootnote)
+    .use(markdownItMark)
+    .use(markdownItSub)
+    .use(markdownItSup)
+    .use(markdownItContainer, "photo-block", {
+      render: function(tokens, idx) {
+        const token = tokens[idx];
+        if (token.nesting === 1) {
+          return `<div class="photo-block">\n`;
+        } else {
+          return `</div>\n`;
+        }
+      }
+    });
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
