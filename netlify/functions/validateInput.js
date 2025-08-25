f1b4adaa0831335c046827e225e24698cd9594cd
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-08-25 16:11:32 -0400

    Added rest of questions and answers

--- netlify/functions/validateInput.js
+++ netlify/functions/validateInput.js
@@ -1,10 +1,18 @@
 const validator = require('email-validator');
 
 const questions = [
-  { id: 0, question: 'What is your favorite color?', answer: 'blue' },
-  { id: 1, question: 'What is your favorite animal?', answer: 'dog' },
-  { id: 2, question: 'What is your favorite food?', answer: 'pizza' }
+  { id: 0, question: 'What is 1+1?', answer: '2' },
+  { id: 1, question: 'What is 3-2?', answer: '1' },
+  { id: 2, question: 'What color is the sky?', answer: 'blue' },
+  { id: 3, question: 'What color are bananas?', answer: 'yellow' },
+  { id: 4, question: 'Type the word: cat', answer: 'cat' },
+  { id: 5, question: 'Type the word: sun', answer: 'sun' },
+  { id: 6, question: 'Is fire hot? (yes or no)', answer: 'yes' },
+  { id: 7, question: 'Is ice warm? (yes or no)', answer: 'no' },
+  { id: 8, question: 'What is 2+2?', answer: '4' },
+  { id: 9, question: 'What color is grass?', answer: 'green' }
 ];
+
 
 function validateInput(name, email, subject, message, phone, phoneArea, bot, question, answer) {
   if (!name || !email || !subject || !message) {
