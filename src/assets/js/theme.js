2a319963ba3547e378baecaee40edfb839aae1a6
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-09-01 18:02:29 -0400

    Fixed problem with sun loading on dark mode if you reload a page

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -3,6 +3,7 @@ const root = document.documentElement;
 
 document.addEventListener("DOMContentLoaded", () => {
   const theme = localStorage.getItem("theme") || "light";
+  changeIcon(theme);
   root.setAttribute("data-theme", theme);
   if (theme === "dark") {
     themeToggle.checked = true;
@@ -13,5 +14,18 @@ themeToggle.addEventListener("change", () => {
   const newTheme = themeToggle.checked ? "dark" : "light";
   root.setAttribute("data-theme", newTheme);
   localStorage.setItem("theme", newTheme);
-  console.log(localStorage.getItem("theme"));
+
 });
+
+function changeIcon(theme) {
+  const icon = document.getElementById("theme-icon");
+
+  if (theme === "dark") {
+    icon.classList.remove("fa-sun")
+    icon.classList.add("fa-moon")
+  } else {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  }
+}
+
